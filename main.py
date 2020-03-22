27120c449f2d95d850923dc3594d681703a9ca34
manavgarg <manavgarg123@gmail.com>
2020-03-21 20:12:43 -0700

    Adding function for printing word cloud.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 
-import argparse
-import json
-import logging
-import time
-
 from collections import defaultdict
+from wordcloud import WordCloud 
+import matplotlib.pyplot as plt 
 
 users= defaultdict(list)
+
+def plotWordCloud(k=50):
+	for user in users:
+		words = ""
+		for tup in users[user]:
+			words += " " + tup[2]
+		wordcloud = WordCloud(width=480, height=480, max_words=k).generate(words)
+		# plot the WordCloud image  
+		plt.figure()
+		plt.title(user) 
+		plt.imshow(wordcloud, interpolation="bilinear") 
+		plt.axis("off") 
+		plt.margins(x=0, y=0) 
+		plt.show() 	
+
 
 if __name__ == '__main__':
 	fd= open("../_chat.txt",mode='r',encoding='utf8', newline='\r\n')
@@ -17,5 +29,8 @@ if __name__ == '__main__':
 		user = line.split("]")[1].split(":")[0].strip()
 		message = line.split("]")[1].split(":")[1]
 		users[user].append((date, time, message))
-	print(users)
+
+	# Compute word cloud (k words) per user.
+	plotWordCloud()	
+
 	fd.close()
