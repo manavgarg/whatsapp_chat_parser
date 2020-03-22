3cb5dcef6ec71a102f000b302c11714f8017adda
manavgarg <manavgarg123@gmail.com>
2020-03-21 21:45:15 -0700

    Adding stopwords.

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 
 from collections import defaultdict
-from wordcloud import WordCloud 
+from wordcloud import WordCloud, STOPWORDS 
 import matplotlib.pyplot as plt 
 
 users= defaultdict(list)
 
 def plotWordCloud(k=50):
+	stopwords = ["image", "omitted", "go", ".", "will"] + list(STOPWORDS) 
+	words = ""
 	for user in users:
-		words = ""
 		for tup in users[user]:
 			words += " " + tup[2].lower()
-		wordcloud = WordCloud(width=480, height=480, max_words=k).generate(words)
-		# plot the WordCloud image  
-		plt.figure()
-		plt.title(user) 
-		plt.imshow(wordcloud, interpolation="bilinear") 
-		plt.axis("off") 
-		plt.margins(x=0, y=0) 
-		plt.show()
-		#plt.savefig(user+".png")
+
+	wordcloud = WordCloud(width=480, height=480, max_words=k,
+		stopwords = stopwords).generate(words)
+	# plot the WordCloud image  
+	plt.figure()
+	plt.imshow(wordcloud, interpolation="bilinear") 
+	plt.axis("off") 
+	plt.margins(x=0, y=0) 
+	plt.show()
+	#plt.savefig("wordcloud.png")
 
 if __name__ == '__main__':
 	fd = open("../_chat.txt",mode='r',encoding='utf8', newline='\r\n')
